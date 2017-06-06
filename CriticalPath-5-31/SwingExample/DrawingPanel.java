7b0a26022da19f24409df5bea91ca3cf07ec2c9d
Yuri <yurivayne@gmail.com>
2017-06-06 14:32:11 -0400

    swing

--- CriticalPath-5-31/SwingExample/DrawingPanel.java
+++ CriticalPath-5-31/SwingExample/DrawingPanel.java
@@ -1,6 +1,10 @@
+package SwingExample;
+
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
 /**
  * Write a description of class DrawingPanel here.
  *
@@ -10,11 +14,21 @@ import java.awt.*;
 public class DrawingPanel extends JPanel
 {
     public DrawingPanel(){
-        setBackground(Color.BLUE);
+        setBackground(Color.GRAY);
+        addMouseListener(new MouseAdapter(){
+            @Override
+            public void mouseClicked(MouseEvent event){
+                System.out.println(String.format("click at: %s,%s",event.getX(),event.getY()));
+            }
+        });
     }
     @Override
 public void paint(Graphics g){
     super.paint(g);
+    g.setColor(Color.yellow);
+    int xCenter = getWidth()/2;
+    int yCenter = getHeight()/2;
+    g.fillOval(xCenter -(50/2),yCenter - (50/2),50,50);
 }
 
 }
--- CriticalPath-5-31/SwingExample/DrawingWindow.java
+++ CriticalPath-5-31/SwingExample/DrawingWindow.java
@@ -1,4 +1,4 @@
-
+package SwingExample;
 import javax.swing.JFrame;
 /**
  * Write a description of class DrawingWindow here.
@@ -12,11 +12,13 @@ public class DrawingWindow extends JFrame
     public DrawingWindow(String title) {
         super(title);
         panel = new DrawingPanel();
+        panel.setSize(50,100);
         //setLayout no usar es mala practica
         getContentPane().add(panel);
     }
     public static void main(String args[]) {
         DrawingWindow window = new DrawingWindow("Drawing Example");
+        window.setSize(500,150);
         window.setVisible(true);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
